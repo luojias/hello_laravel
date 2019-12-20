27b9c81086bd7ff53c5d61f26025681578612714
LuoJia <176064056@qq.com>
2019-12-20 09:19:47 +0000

    git get

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Luojia</div>
+                <div class="title">Hello Laravel! - by Luojia-git get</div>
             </div>
         </div>
     </body>
